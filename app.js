b1e9b66a9c5c6a368a6ee1c1c878fff837967b38
shin☘ <huutrimega@gmail.com>
2020-12-27 16:11:09 +0700

    update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/"));
+app.get("/", (req, res) => {
+	res.redirect("/src/index.html");
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}/src/index.html`);
